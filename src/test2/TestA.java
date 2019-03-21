698d417d0166579218711ce6654b38e67113e2cd
jackliuhao <jackliuhao@123>
2019-03-21 11:11:36 +0800

    q-update

--- src/test2/TestA.java
+++ src/test2/TestA.java
@@ -2,6 +2,6 @@ package test2;
 
 public class TestA {
 	public static void main(String[] args) {
-		System.out.println("hello world~~~~~~~");
+		System.out.println("hello world~~~~~~~#####");
 	}
 }
